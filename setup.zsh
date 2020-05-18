c332ba597961f4b0cf95734865e00e1049c9489c
William <williambbarnhart@gmail.com>
2020-05-17 21:01:26 -0400

    Automated wget installation

--- setup.zsh
+++ setup.zsh
@@ -2,3 +2,10 @@
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 pip install pyenchant wget shutil requests urlparse htmlparser pyside2
+curl -O http://ftp.gnu.org/gnu/wget/wget-1.16.3.tar.gz
+tar -xzf wget-1.16.3.tar.gz
+cd wget-1.16.3
+./configure --with-ssl=openssl
+make
+sudo make install
+cd .. && rm -rf wget*
