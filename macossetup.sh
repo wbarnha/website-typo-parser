c85b5776f7a82eb1033118d93922904a60a5ddc1
wbarnha <williambbarnhart@gmail.com>
2020-06-04 19:27:05 -0400

    Simplified wget setup

--- macossetup.sh
+++ macossetup.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install pkg-config
+brew install pkg-config gnutls wget
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 pip install pyenchant wget requests htmlparser pyside2
-curl -O http://ftp.gnu.org/gnu/wget/wget-1.16.3.tar.gz
-tar -xzf wget-1.16.3.tar.gz
-cd wget-1.16.3
-./configure --with-ssl=openssl
-make
-sudo make install
-cd .. && rm -rf wget*
