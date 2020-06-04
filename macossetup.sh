a90681cceb51e1c5795f22f43d9e3d556ca332fb
wbarnha <williambbarnhart@gmail.com>
2020-06-04 19:14:29 -0400

    Include Brew setup

--- macossetup.sh
+++ macossetup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install pkg-config
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 pip install pyenchant wget requests htmlparser pyside2
