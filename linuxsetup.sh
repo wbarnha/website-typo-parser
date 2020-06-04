496a68281dc96aaecd6a285668ab3914ef01d978
wbarnha <williambbarnhart@gmail.com>
2020-06-04 19:15:52 -0400

    Removed unnecessary dependencies

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # For setting up your Python 2 environment for typoparser.py
 sudo apt install python-pip
-sudo pip install pyenchant wget shutil requests urlparse htmlparser pyside2
+sudo pip install pyenchant wget requests htmlparser pyside2
