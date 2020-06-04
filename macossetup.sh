ee0fa58f24976aedb77c6b08dd2d9032fa0f02aa
wbarnha <williambbarnhart@gmail.com>
2020-06-04 19:04:34 -0400

    Removed unnecessary dependencies from macOS setup

--- macossetup.sh
+++ macossetup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-pip install pyenchant wget shutil requests urlparse htmlparser pyside2
+sudo python get-pip.py
+pip install pyenchant wget requests htmlparser pyside2
 curl -O http://ftp.gnu.org/gnu/wget/wget-1.16.3.tar.gz
 tar -xzf wget-1.16.3.tar.gz
 cd wget-1.16.3
